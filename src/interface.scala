d6a23c95218524958c174b4ce793ed97a20bd5d1
Toby Cathcart Burn <tcathcartburn@gmail.com>
2016-11-19 13:12:31 +0000

    started manager

--- src/interface.scala
+++ src/interface.scala
@@ -3,5 +3,18 @@
   */
 
 trait Manager{
-  def hello(cash:Int,syms:List[(sym,Int)])
+  def hello(cash:Int,syms:List[(Sym,Int)])
+  //HELLO CASH SYM:POSN SYM:POSN ...
+  //OPEN SYM SYM SYM ...
+  def open(List(Sym))
+  //CLOSE SYM SYM SYM ...
+  def close(List(Sym))
+  //ERROR MSG
+
+  //  BOOK SYMBOL BUY PRICE:SIZE PRICE:SIZE ... SELL PRICE:SIZE PRICE:SIZE ...
+  //TRADE SYMBOL PRICE SIZE
+  //  ACK ID
+   // REJECT ID MSG
+  //FILL ID SYMBOL DIR PRICE SIZE
+  //  OUT ID
 }
