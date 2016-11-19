770e2db315ceea8589e735386bf3173671441727
sv26031 <sonal_vedi@hotmail.com>
2016-11-19 14:37:22 +0000

    concrete sender implemented but untested. fingers crossed

---
+++ src/ConcreteSender.scala
@@ -0,0 +1,31 @@
+import java.io.PrintWriter
+import java.net.Socket
+
+/**
+  * Created by Sonal on 19/11/2016.
+  */
+class ConcreteSender extends Sender {
+
+  val skt = Bot.skt
+  val to_exchange = new PrintWriter(skt.getOutputStream, true)
+
+  override def hello(teamname: String): Unit = {
+//    Parameter is irrelevant because we're team apricot
+    to_exchange.println("HELLO APRICOT")
+  }
+
+  override def add(id: Int, sym: Sym, buy: Boolean, price: Int, size: Int): Unit = {
+    var str = s"ADD $id $sym ${if (buy) "BUY" else "SELL"} $price $size"
+    to_exchange.println(str)
+  }
+
+  override def convert(id: Int, sym: Sym, buy: Boolean, size: Int): Unit = {
+    var str = s"CONVERT $id $sym ${if (buy) "BUY" else "SELL"} $size"
+    to_exchange.println(str)
+  }
+
+  override def cancel(id: Int): Unit = {
+    var str = s"CANCEL $id"
+    to_exchange.println(str)
+  }
+}
