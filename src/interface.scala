cbf6b473677f375baf0cbec8a437c1fb99d737ab
Toby Cathcart Burn <tcathcartburn@gmail.com>
2016-11-19 13:33:14 +0000

    more on manager

--- src/interface.scala
+++ src/interface.scala
@@ -2,19 +2,30 @@
   * Created by Toby Cathcart Burn on 19/11/2016.
   */
 
+
+type Sym=String
+
 trait Manager{
-  def hello(cash:Int,syms:List[(Sym,Int)])
   //HELLO CASH SYM:POSN SYM:POSN ...
+  def hello(cash:Int,syms:Array[(Sym,Int)])
   //OPEN SYM SYM SYM ...
-  def open(List(Sym))
+  def open(syms:Array[Sym])
   //CLOSE SYM SYM SYM ...
-  def close(List(Sym))
+  def close(syms:Array[Sym])
   //ERROR MSG
+  def error(msg:String)
 
   //  BOOK SYMBOL BUY PRICE:SIZE PRICE:SIZE ... SELL PRICE:SIZE PRICE:SIZE ...
+  def book(sym :Sym,buys:Array[(Int,Int)],sells:Array[(Int,Int)])
   //TRADE SYMBOL PRICE SIZE
+  def trade(Sym,Array[(Int,Int)],Array[(Int,Int)])
+
   //  ACK ID
+  def error(Sym,Array[(Int,Int)],Array[(Int,Int)])
    // REJECT ID MSG
+   def reject(Int,String)
   //FILL ID SYMBOL DIR PRICE SIZE
-  //  OUT ID
+  def fill(id:Int,)
+  //  OUT ID --not sure what it does
+  def out(Int)
 }
